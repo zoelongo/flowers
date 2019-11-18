f96d0a01c1c03d119dfa3cfe22c2839eb933c88f
Zoe Longo <zoelongo@yahoo.com>
2019-11-17 18:38:26 -0700

    going to revert

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -3,7 +3,7 @@ import "./Header.css";
 
 const Header = props => (
   <div className="header">
-    <div className="title">{props.children}</div>
+    <div className="title">Flower Clicky</div>
     <div className="scores">
       Score: {props.score} Highscore: {props.highscore}
     </div>
